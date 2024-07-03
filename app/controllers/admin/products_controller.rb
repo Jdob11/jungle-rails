a38731442246e7240f6f7f8cdccabb54230eef7d
Jdob11 <jbdobson11@gmail.com>
2024-07-03 10:44:01 -0700

    feat: Add basic http auth to admin products controller

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -35,6 +36,15 @@ class Admin::ProductsController < ApplicationController
       :image,
       :price
     )
+  end
+
+  def authenticate
+    username = ENV['HTTP_BASIC_AUTH_USERNAME']
+    password = ENV['HTTP_BASIC_AUTH_PASSWORD']
+
+    authenticate_or_request_with_http_basic do |provided_username, provided_password|
+      provided_username == username && provided_password == password
+    end
   end
 
 end
