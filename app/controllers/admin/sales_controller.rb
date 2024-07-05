928d746ec756ed01d923ef4c4cf0ee34a0799471
Jdob11 <jbdobson11@gmail.com>
2024-07-05 16:37:02 -0700

    feat: Change alert to dynamically load sale name and percent

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,7 +1,17 @@
 class Admin::SalesController < ApplicationController
+  include SalesHelper
 
   def index
     @sales = Sale.all
+    if active_sale?
+      @active_sale = Sale.active.first
+    end
+  end
+
+  def destroy
+    @sale = Sale.find params[:id]
+    @sale.destroy
+    redirect_to [:admin, :sales], notice: 'Sale deleted!'
   end
 
 end
