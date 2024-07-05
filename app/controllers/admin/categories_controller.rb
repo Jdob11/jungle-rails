39912a85292c3b83e95598bd488b5acadde92ee0
Jdob11 <jbdobson11@gmail.com>
2024-07-04 17:02:51 -0700

    feat(admin): Add redirect to create method to bring user back to categories page on success

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,12 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Product.new(category_params)
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
   private
