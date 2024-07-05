c1b2835fd16d197f78eda17edd8e281b99b94392
Jdob11 <jbdobson11@gmail.com>
2024-07-05 11:31:34 -0700

    feat: Create user controller, user views, migrate user table to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
+  get '/signup', to: 'users#new'
+  resources :users, only: [:index, :new, :create]
   get 'about', to: 'about#index'
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
