32ca1bb9fe9f3059a2c7e8a3dc3a97f08c6a7cfd
Jdob11 <jbdobson11@gmail.com>
2024-07-05 11:48:54 -0700

    feat: Create routes for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get '/login', to: 'sessions#new' 
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/signup', to: 'users#new'
   resources :users, only: [:index, :new, :create]
+  post '/users', to: 'users#create'
   get 'about', to: 'about#index'
   root to: 'products#index'
 
