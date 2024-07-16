d908524244ded74a90802fb01bc6e12deab2a416
Jdob11 <jbdobson11@gmail.com>
2024-07-16 15:15:00 -0700

    test: Add includes product.errors.full_messages expectation to remaining validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,19 +24,20 @@ RSpec.describe Product, type: :model do
     
     it 'should fail when price is not given' do
       product = Product.new(valid_attributes.merge(price_cents: nil))
-      product.valid?
-      puts product.errors.full_messages
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'should fail when quantity is not given' do
       product = Product.new(valid_attributes.merge(quantity: nil))
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'should fail when category is not given' do
       product = Product.new(valid_attributes.merge(category: nil))
       expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
