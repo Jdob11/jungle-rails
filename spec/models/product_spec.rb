2c6602b81c18e3529acfa038366cd1c1ae08cf65
Jdob11 <jbdobson11@gmail.com>
2024-07-16 15:04:39 -0700

    test: Add price validation test for Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
     before(:each) do
       @category = Category.create(name: 'Weeds')
     end
 
+    let(:valid_attributes) do
+      { name: 'Dandelion', price_cents: 500, quantity: 20000, category: @category }
+    end
+
     it 'should save when all fields are given' do
-      product = Product.new(name: 'Dandelion', price: 500, quantity: 20000, category: @category)
+      product = Product.new(valid_attributes)
       expect(product).to be_valid
     end
 
     it 'should fail when name is not given' do
-      product = Product.new(name: nil, price: 500, quantity: 20000, category: @category)
+      product = Product.new(valid_attributes.merge(name: nil))
       expect(product).not_to be_valid
     end
 
-    it 'should fail when name is not given' do
-      product = Product.new(name: nil, price: 500, quantity: 20000, category: @category)
+    it 'should fail when price is not given' do
+      product = Product.new(valid_attributes.merge(price_cents: nil))
       expect(product).not_to be_valid
     end
   end
