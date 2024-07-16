c1b9c33aff52d7f271b58af0f57266403dadcb6f
Jdob11 <jbdobson11@gmail.com>
2024-07-16 15:52:07 -0700

    feat: Add authenticate_with_credentials method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials(email, password)
+    cleaned_email = email.strip.downcase
+    user = User.find_by(email: cleaned_email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
